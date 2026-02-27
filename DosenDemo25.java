62be414f39ff19928bb843a164a02aafbd55551c
Rizal Maulana <xjunngoat1@gmail.com>
2026-02-27 09:39:22 +0700

    Tugas 1 dan 2

---
+++ DosenDemo25.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class DosenDemo25 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan Jumlah Dosen: ");
+        int jumlah = sc.nextInt();
+        sc.nextLine();
+
+        Dosen25[] arrayDosen = new Dosen25[jumlah];
+
+        for (int i = 0; i < arrayDosen.length; i++) {
+            System.out.println("Masukkan Data Dosen ke-" + (i + 1));
+            System.out.print("Kode          : ");
+            String kode = sc.nextLine();
+            System.out.print("Nama          : ");
+            String nama = sc.nextLine();
+            System.out.print("Jenis Kelamin : ");
+            String jK = sc.nextLine();
+            boolean jenisKelamin;
+            if (jK.equalsIgnoreCase("Pria")) {
+                jenisKelamin = true;
+            } else {
+                jenisKelamin = false;
+            }
+            System.out.print("Usia          : ");
+            int usia = sc.nextInt();
+            sc.nextLine();
+            System.out.println("-----------------------");
+
+            arrayDosen[i] = new Dosen25(kode, nama, jenisKelamin, usia);
+        }
+        
+        int i = 1;
+        for (Dosen25 dosen : arrayDosen) {
+            System.out.println("Data Dosen ke-" + i++);
+            dosen.cetakData();
+            System.out.println("-----------------------");
+        }
+
+        DataDosen25 data = new DataDosen25();
+
+        System.out.println("\n===== Data Seluruh Dosen =====");
+        data.dataSemuaDosen(arrayDosen);
+        System.out.println("\n===== Jumlah Dosen Setiap Jenis Kelamin =====");
+        data.jumlahDosenSetiapJenisKelamin(arrayDosen);
+        System.out.println("\n===== Rata-rata Usia Setiap Jenis kelamin =====");
+        data.rataRataUsiaDosen(arrayDosen);
+        System.out.println("\n===== Dosen Paling Tua =====");
+        data.dosenPalingTua(arrayDosen);
+        System.out.println("\n===== Dosen Paling Muda =====");
+        data.dosenPalingMuda(arrayDosen);
+    }
+}

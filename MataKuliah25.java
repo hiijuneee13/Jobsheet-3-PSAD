21f3045b2528a4a1785d3f1fa463e2dbfb1bf609
Rizal Maulana <xjunngoat1@gmail.com>
2026-02-27 09:01:14 +0700

    Modifikasi Pertanyaayn percobaan 3 no 2

--- MataKuliah25.java
+++ MataKuliah25.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class MataKuliah25 {
     public String kode;
     public String nama;
@@ -9,5 +10,18 @@ public class MataKuliah25 {
         this.nama = nama;
         this.sks =  sks;
         this.jumlahJam = jumlahJam;
+    }
+
+    void tambahData() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Kode Matakuliah  : ");
+        kode = sc.nextLine();
+        System.out.print("Nama Matakuliah  : ");
+        nama = sc.nextLine();
+        System.out.print("SKS              : ");
+        sks = sc.nextInt();
+        System.out.print("Jumlah Jam       : ");
+        jumlahJam = sc.nextInt();
     }
 }
